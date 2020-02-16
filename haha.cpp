19ec4523549768ec0a9aeec5fba89e6ff5f08aca
hehehahaha <gejiexian@126.com>
2020-02-16 11:01:07 +0800

    Update haha.cpp

--- haha.cpp
+++ haha.cpp
@@ -8,18 +8,18 @@ for (int r = 0; r < OUT_HEIGHT_Conv; r++)   //24
     for (int kr = 0; kr < Conv_MAP_SIZE; kr++)    //5
     {
       Kernel_Column:
-			for (int kc = 0; kc < Conv_MAP_SIZE; kc++)    //5
-			{
+      for (int kc = 0; kc < Conv_MAP_SIZE; kc++)    //5
+      {
         Out_Channel:
-				for (int cho = 0; cho < OUT_CH_NUM_Conv; cho++)    //8
-				{
+        for (int cho = 0; cho < OUT_CH_NUM_Conv; cho++)    //8
+	{
           Input_Channel:
-					for (int chi = 0; chi < IN_CH_NUM_Conv; chi++)     //1
-					{
-						ImgOut[cho][r][c] += ImgIn[chi][r + kr][c + kc] * weight[cho][chi][kr][kc];
-					}
-				}
-			}
-		}
-	}
+	  for (int chi = 0; chi < IN_CH_NUM_Conv; chi++)     //1
+	  {
+             ImgOut[cho][r][c] += ImgIn[chi][r + kr][c + kc] * weight[cho][chi][kr][kc];
+	  }
+        }
+      }  
+    }
+  }
 }
