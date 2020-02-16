1901be84bbe8b8446dd87cae3633ca06332e1a24
hehehahaha <gejiexian@126.com>
2020-02-16 10:59:58 +0800

    Update haha.cpp

--- haha.cpp
+++ haha.cpp
@@ -3,10 +3,10 @@ for (int r = 0; r < OUT_HEIGHT_Conv; r++)   //24
 {
   Column:
   for (int c = 0; c < OUT_WIDTH_Conv; c++)   //24
-	{
+  {
     Kernel_Row:
-		for (int kr = 0; kr < Conv_MAP_SIZE; kr++)    //5
-		{
+    for (int kr = 0; kr < Conv_MAP_SIZE; kr++)    //5
+    {
       Kernel_Column:
 			for (int kc = 0; kc < Conv_MAP_SIZE; kc++)    //5
 			{
