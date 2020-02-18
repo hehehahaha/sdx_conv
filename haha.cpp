73f2704a78abea5031b2926d7dd7fc5b95b58a5b
hehehahaha <gejiexian@126.com>
2020-02-18 10:47:51 +0800

    Update haha.cpp

--- haha.cpp
+++ haha.cpp
@@ -54,3 +54,33 @@ for (int r = 0; r < OUT_HEIGHT_Conv; r++)   // OUT_HEIGHT_Conv = 24
 
 
 
+#pragma HLS array_partition variable = ImgIn complete dim = 1
+#pragma HLS array_partition variable = ImgOut complete dim = 1
+#pragma HLS array_partition variable = weight complete dim = 1
+#pragma HLS array_partition variable = weight complete dim = 2
+
+for (int r = 0; r < OUT_HEIGHT_Conv; r++)   // OUT_HEIGHT_Conv = 24
+{
+  for (int c = 0; c < OUT_WIDTH_Conv; c++)   // OUT_WIDTH_Conv = 24
+  {
+    for (int kr = 0; kr < Conv_MAP_SIZE; kr++)    // Conv_MAP_SIZE = 5
+    {
+      for (int kc = 0; kc < Conv_MAP_SIZE; kc++)    // Conv_MAP_SIZE = 5
+      {
+#pragma HLS PIPELINE
+        for (int cho = 0; cho < OUT_CH_NUM_Conv; cho++)    // OUT_CH_NUM_Conv = 32
+	{
+	  for (int chi = 0; chi < IN_CH_NUM_Conv; chi++)     // IN_CH_NUM_Conv = 1
+	  {
+             ImgOut[cho][r][c] += ImgIn[chi][r + kr][c + kc] * Weight[cho][chi][kr][kc];
+	  }
+        }
+      }  
+    }
+  }
+}
+
+
+
+
+
