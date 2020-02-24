5790e7438297103985d8b2fe38ff05461b14b46e
hehehahaha <gejiexian@126.com>
2020-02-24 16:24:05 +0800

    Update haha.cpp

--- haha.cpp
+++ haha.cpp
@@ -83,35 +83,35 @@ for (int r = 0; r < OUT_HEIGHT_Conv; r++)   // OUT_HEIGHT_Conv = 24
 
 
 for (int cyc = 0; cyc < OUT_CH_NUM_Conv; cyc += 4)
- {
-  memcpy((void*)tempA, (const void*)A, sizeof(haha) * 28 * 28);
-  memcpy((void*)tempB, B + 5 * 5 * 1 * 4 * cyc / 4, sizeof(haha) * 5 * 5 * 1 * 4);
-
-  for (int kr = 0; kr < Conv_MAP_SIZE; kr++)//5
+ {	
+  memcpy(ImgIn, In, sizeof(float) * 28 * 28);
+  memcpy(Weight, W + 5 * 5 * 1 * 4 * cyc / 4, sizeof(float) * 5 * 5 * 1 * 4);
+  for (int r = 0; r < OUT_HEIGHT_Conv; r++)   // OUT_HEIGHT_Conv = 24
   {
-   for (int kc = 0; kc < Conv_MAP_SIZE; kc++)//5
-   {
-    for (int r = 0; r < OUT_HEIGHT_Conv; r++)//24
+    for (int c = 0; c < OUT_WIDTH_Conv; c++)   // OUT_WIDTH_Conv = 24
     {
-     for (int c = 0; c < OUT_WIDTH_Conv; c++)//24
-     {
-      for (int cho = 0; cho < OUT_CH_NUM_Conv/2; cho++)//8
+      for (int kr = 0; kr < Conv_MAP_SIZE; kr++)    // Conv_MAP_SIZE = 5
       {
- for (int chi = 0; chi < IN_CH_NUM_Conv; chi++)//1
-							{
-								tempconv1out[cho][r][c] += tempA[chi][r + kr][c + kc] * tempB[cho][chi][kr][kc];
-							}
-						}
-					}
-				}
-			}
-		}
-		memcpy(conv1out + 4 * 24 * 24 * cyc / 4, (const void*)tempconv1out, sizeof(haha) * 4 * 24 * 24);
-		for (int i = 0; i < OUT_CH_NUM_Conv / 2; i++) {
-			for (int j = 0; j < OUT_HEIGHT_Conv; j++) {
-				for (int k = 0; k < OUT_WIDTH_Conv; k++) {
-					tempconv1out[i][j][k] = 0;
-				}
-			}
-		}
+        for (int kc = 0; kc < Conv_MAP_SIZE; kc++)    // Conv_MAP_SIZE = 5
+        {
+#pragma HLS PIPELINE
+          for (int cho = 0; cho < OUT_CH_NUM_Conv; cho++)    // OUT_CH_NUM_Conv = 32
+	  {
+	    for (int chi = 0; chi < IN_CH_NUM_Conv; chi++)     // IN_CH_NUM_Conv = 1
+	    {
+              ImgOut[cho][r][c] += ImgIn[chi][r + kr][c + kc] * Weight[cho][chi][kr][kc];
+	    }
+          }
+        }  
+      }
+    }
+  }		
+  memcpy(Out + 4 * 24 * 24 * cyc / 4, ImgOut, sizeof(float) * 4 * 24 * 24);
+  for (int i = 0; i < OUT_CH_NUM_Conv / 2; i++) {
+    for (int j = 0; j < OUT_HEIGHT_Conv; j++) {
+      for (int k = 0; k < OUT_WIDTH_Conv; k++) {
+	tempconv1out[i][j][k] = 0;
+      }
+    }
+  }
 
